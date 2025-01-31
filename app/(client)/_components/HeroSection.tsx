cf3950551cf112a1f85b14a42a2bd9d206cf9ab8
Jahid0178 <zahidul.haque767@gmail.com>
2025-02-01 04:09:02 +0600

    feat: Add home page hero section animation

---
+++ app/(client)/_components/HeroSection.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import Button from "@/components/Button/Button";
+
+const HeroSection = () => {
+  return (
+    <div className="h-[92vh] text-white relative">
+      <div className="absolute top-0 right-0 bottom-0 bg-hero-image w-[50%] h-full">
+        <Image
+          src={"/images/hero-bg.avif"}
+          alt="Hero Background"
+          width={600}
+          height={600}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="container relative h-full flex items-center z-50">
+        <div className="max-w-2xl space-y-6">
+          <h1 className="text-7xl font-semibold">
+            Embrace the future of finance
+          </h1>
+          <p className="font-semibold">
+            Reimagine financial services with AnyTech&apos;s open platform,
+            distributed banking solution that powers transformation
+          </p>
+          <Button size="lg">Reach Out to Us</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -1,14 +1,19 @@
 import React from "react";
+import HeroSection from "./_components/HeroSection";
 import PhilosophySection from "./_components/PhilosophySection";
 import CustomerSolutionsSection from "./_components/CustomerSolutionsSection";
 import WaveDivider from "@/components/svg/WaveDivider";
 import TrustedPartnersSection from "./_components/TrustedPartnersSection";
 import ContactBanner from "./_components/ContactBanner";
 import InnovativeFinanceSection from "./_components/InnovativeFinanceSection";
+import BGAnimateWrapper from "./_components/BGAnimateWrapper";
 
 const HomePage = () => {
   return (
     <>
+      <BGAnimateWrapper>
+        <HeroSection />
+      </BGAnimateWrapper>
       <InnovativeFinanceSection />
       <WaveDivider />
       <PhilosophySection />
